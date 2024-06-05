a89ca00ebf5eab2982284a88b859fc2f9024d543
Szymon Węgrzynek <u105_szyweg_waw@technischools.com>
2024-06-06 00:01:24 +0200

    change text color depends on connection

--- src/components/Device/Device.jsx
+++ src/components/Device/Device.jsx
@@ -1,49 +1,70 @@
 import { useWebSocket } from "../../api/ws";
 import "./style.scss";
 
-const Device = ({device}) => {
-    const { name, status, is_light_on, last_update, uptime, id } = device;
-    const { handleLightModeChange, handleReconnect, handleDisconnect } = useWebSocket();
+const Device = ({ device }) => {
+  const { name, status, is_light_on, last_update, uptime, id } = device;
+  const { handleLightModeChange, handleReconnect, handleDisconnect } =
+    useWebSocket();
 
-    const statusName = status ? "Connected" : "Disconnected";
-    const LightMode = is_light_on ? "On" : "Off";
-    const date = new Date(last_update * 1000).toLocaleString();
+  const statusName = status ? "Connected" : "Disconnected";
+  const LightMode = is_light_on ? "On" : "Off";
+  const date = new Date(last_update * 1000).toLocaleString();
 
+  function handleConnectClick() {
+    handleReconnect(id);
+  }
 
-    function handleConnectClick(){
-        handleReconnect(id)
-    }
-    
+  function handleLightClick() {
+    handleLightModeChange(id);
+  }
 
-    function handleLightClick(){
-        handleLightModeChange(id)
-    }
+  function handleDisconnectClick() {
+    handleDisconnect(id);
+  }
 
-
-    function handleDisconnectClick(){
-        handleDisconnect(id)
-    };
-
-    
-    return (
-        <article className="device">
-            <div>
-                <h1>{name}</h1>
-            </div>
-            <div>
-                <p>Updated at: <span>{date}</span></p>
-                <p>Uptime: <span>{uptime}</span></p>
-            </div>
-            <div>
-                <p>Status: <span>{statusName}</span></p>
-                <p>Light mode: <span>{LightMode}</span></p>
-            </div>
-            <div>
-                {status ? <button onClick={handleDisconnectClick}>Disconnect</button> : <button onClick={handleConnectClick}>Connect</button>}
-                <button disabled={!status} onClick={handleLightClick}>Change light mode</button>
-            </div>
-        </article>
-    );
+  return (
+    <article className="device">
+      <div>
+        <h1>{name}</h1>
+      </div>
+      <div>
+        <p>
+          Updated at:
+          <span className={status ? "connected" : "disconnected"}>{date}</span>
+        </p>
+        <p>
+          Uptime:
+          <span className={status ? "connected" : "disconnected"}>
+            {uptime}
+          </span>
+        </p>
+      </div>
+      <div>
+        <p>
+          Status:
+          <span className={status ? "connected" : "disconnected"}>
+            {statusName}
+          </span>
+        </p>
+        <p>
+          Light mode:
+          <span className={status ? "connected" : "disconnected"}>
+            {LightMode}
+          </span>
+        </p>
+      </div>
+      <div>
+        {status ? (
+          <button onClick={handleDisconnectClick}>Disconnect</button>
+        ) : (
+          <button onClick={handleConnectClick}>Connect</button>
+        )}
+        <button disabled={!status} onClick={handleLightClick}>
+          Change light mode
+        </button>
+      </div>
+    </article>
+  );
 };
 
 export default Device;
